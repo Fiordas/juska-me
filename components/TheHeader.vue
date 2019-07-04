8dfa163e4ea2f24a3634a5caf94f8da987abd1e0
Fiordas <simonas.juska@outlook.com>
2019-07-04 22:27:51 +0300

    Update Header component

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,35 +1,70 @@
 <template>
-  <nav class="navbar has-shadow">
+  <nav class="navbar has-shadow is-fixed-top">
     <div class="container">
       <div class="navbar-brand">
-        <nuxt-link to="/" class="logo-text navbar-item">Simonas
-          <span class="logo-color">Juska</span>
+        <a href="#" v-scroll-to="'#top'" class="logo-text navbar-item has-text-dark" v-if="$route.name === 'index'">
+          Simonas
+          <span class="has-text-primary">Juska</span>
+        </a>
+        <nuxt-link to="/" class="logo-text navbar-item" v-else>
+          Simonas
+          <span class="has-text-primary">Juska</span>
         </nuxt-link>
-        <div class="navbar-burger burger">
+        <div class="navbar-burger burger" :class="{'is-active': isBurger}" @click="isBurger = !isBurger">
           <span></span>
           <span></span>
           <span></span>
         </div>
       </div>
-      <div class="navbar-menu">
-        <div class="navbar-end">
-          <nuxt-link to="about" class="navbar-item">About</nuxt-link>
-          <nuxt-link to="work" class="navbar-item">Work</nuxt-link>
-          <nuxt-link to="contact" class="navbar-item">Contact</nuxt-link>
+      <div class="navbar-menu" :class="{'is-active': isBurger}">
+        <div class="navbar-end" v-if="$route.name === 'index'">
+          <div class="navbar-item">
+            <a href="#" v-scroll-to="'#about'" class="navbar-item" @click="isBurger ? isBurger = !isBurger: null">
+              <strong>About</strong>
+            </a>
+          </div>
+          <div class="navbar-item">
+            <a href="#" v-scroll-to="'#work'" class="navbar-item" @click="isBurger ? isBurger = !isBurger: null">
+              <strong>Portfolio</strong>
+            </a>
+          </div>
+          <div class="navbar-item">
+            <a href="#" v-scroll-to="'#contact'" class="navbar-item" @click="isBurger ? isBurger = !isBurger: null">
+              <strong>Contact</strong>
+            </a>
+          </div>
+        </div>
+        <div class="navbar-end" v-else>
+          <div class="navbar-item">
+            <button @click="$router.go(-1)" class="button is-primary is-outlined">
+              <span class="icon is-medium">
+                <fa :icon="['fas', 'undo']" />
+              </span>
+              <strong>Back</strong>
+            </button>
+          </div>
         </div>
       </div>
     </div>
   </nav>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      isBurger: false
+    }
+  }
+}
+</script>
+
+
 <style scoped>
-@import url("https://fonts.googleapis.com/css?family=Kanit:700");
+@import url('https://fonts.googleapis.com/css?family=Kanit:700');
 
 .logo-text {
-  font-family: "Kanit", sans-serif;
+  font-family: 'Kanit', sans-serif;
   font-size: 1.5rem;
-}
-.logo-color {
-  color: rgb(81, 86, 172);
 }
 </style>
